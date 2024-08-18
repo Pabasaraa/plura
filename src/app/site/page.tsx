308824c17aa585a78be5508dbaff11e14e357a03
Pabasaraa <pabasara.was@gmail.com>
2024-08-18 22:40:14 +0530

    add middleware and agency creation feature

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -15,7 +15,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <section className="h-full w-full md:pt-36 relative flex items-center justify-center flex-col">
+      <section className="md:pt-24 relative flex items-center justify-center flex-col">
         {/* checked background displayed in the dark mode */}
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] z-[-10]" />
         <p className="text-center">Run your agency, in one place</p>
@@ -34,7 +34,7 @@ export default function Home() {
             width={1200}
             className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
           />
-          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+          <div className="bottom-0 top-[50%] bg-gradient-to-t from-background left-0 right-0 absolute z-10"></div>
         </div>
       </section>
       <section className="flex justify-center flex-col items-center gap-4 md:!mt-20 mt-[-60px]">
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,70 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import {
+  clerkMiddleware,
+  createRouteMatcher,
+  ClerkMiddlewareAuth,
+} from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/site",
+  "/api/uploadthing",
+  "/agency/sign-in(.*)",
+  "/agency/sign-up(.*)",
+]);
+
+const afterAuth = async (auth: ClerkMiddlewareAuth, req: NextRequest) => {
+  //rewrite for domains
+  const url = req.nextUrl;
+  const searchParams = url.searchParams.toString();
+  let hostname = req.headers;
+
+  //url -> localhost:3000/site?h=200
+  //pathWithSearchParams -> /site?h=200
+  const pathWithSearchParams = `${url.pathname}${
+    searchParams.length > 0 ? `?${searchParams}` : ""
+  }`;
+
+  //host -> test.localhost:3000/site
+  //customSubDomain -> test.
+  const customSubdomain = hostname
+    .get("host")
+    ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+    .filter(Boolean)[0];
+
+  //if subdomain exists
+  if (customSubdomain) {
+    return NextResponse.rewrite(
+      new URL(`/${customSubdomain}${pathWithSearchParams}`, req.url)
+    );
+  }
+
+  if (url.pathname === "/sign-in" || url.pathname === "sign-up") {
+    return NextResponse.redirect(new URL("/agency/sign-in", req.url));
+  }
+
+  // why do we need to verify whether url.host is equals to NEXT_PUBLIC_DOMAIN?
+  if (
+    url.pathname === "/" ||
+    (url.pathname === "/site" && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+  ) {
+    return NextResponse.rewrite(new URL("/site", req.url));
+  }
+
+  if (
+    url.pathname.startsWith("/agency") ||
+    url.pathname.startsWith("/subaccount")
+  ) {
+    return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url));
+  }
+};
+
+export default clerkMiddleware((auth, req) => {
+  // if (!isPublicRoute(req)) {
+  //   auth().protect();
+  // }
+
+  return afterAuth(auth, req);
+});
 
 export const config = {
   matcher: [
